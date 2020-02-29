aad0b1047034636314ceb62cb54497e00a2e6580
Mark Du <dulongjie@gmail.com>
2020-02-29 11:49:28 -0500

    comment added

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render
 from .forms import AppointmentForm
 
@@ -15,14 +16,15 @@ def appointment(request):
         filled_form = AppointmentForm(request.POST)
         if filled_form.is_valid():
             filled_form.save()
-            note = 'Thanks for booking! Engineer %s will visit you at %s !' % (
-                filled_form.cleaned_data['engineer'], filled_form.cleaned_data['date'],
-                )
+            messages.success(request, 'Appointment booked') \
+                # = 'Thanks for booking! Engineer %s will visit you at %s !' % (
+                # filled_form.cleaned_data['engineer'], filled_form.cleaned_data['date'],
+                # )
         else:
             note = 'Appointment was not created, please try again'
         new_form = AppointmentForm()
         return render(request, 'job/appointment.html',
-                      {'AppointmentForm': new_form, 'note': note})
+                      {'AppointmentForm': new_form})
     else:
         form = AppointmentForm()
         return render(request, 'job/appointment.html', {'AppointmentForm': form})
